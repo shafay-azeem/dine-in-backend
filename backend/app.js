6ac12b9ca85239c964083a893563ebbca6f3e16c
faizan-99 <mfaizy605@gmail.com>
2023-01-06 20:26:14 +0500

    updated

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 const menu = require("./routes/MenuRoute");
 const feedback = require("./routes/FeedbackRoutes");
