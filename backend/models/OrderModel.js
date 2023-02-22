b9aee451ff43f7edc2bf23ed94bf668de6e2caa3
faizan-99 <mfaizy605@gmail.com>
2023-02-22 20:48:28 +0500

    updated fields

--- backend/models/OrderModel.js
+++ backend/models/OrderModel.js
@@ -1,54 +1,56 @@
 const mongoose = require("mongoose");
 
-const OrderSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.ObjectId,
-    ref: "User",
-  },
-  customerName: {
-    type: String,
-  },
-  tableNumber: {
-    type: Number,
-  },
-  orderedItems: [
-    {
-      itemId: {
-        type: mongoose.Schema.ObjectId,
-        ref: "Item",
-      },
-      itemName: {
-        type: String,
-      },
-      itemPrice: {
-        type: Number,
-      },
-      quantity: {
-        type: Number,
-      },
-      itemImg: {
-        type: String,
-      },
-      itemPrice_Total: {
-        type: Number,
-      },
+const OrderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
     },
-  ],
+    customerName: {
+      type: String,
+    },
+    tableNumber: {
+      type: Number,
+    },
+    orderedItems: [
+      {
+        item_Id: {
+          type: mongoose.Schema.ObjectId,
+          ref: "Item",
+        },
+        item_Name: {
+          type: String,
+        },
+        item_Price: {
+          type: Number,
+        },
+        item_Qty: {
+          type: Number,
+        },
+        item_Img: {
+          type: String,
+        },
+        itemPrice_Total: {
+          type: Number,
+        },
+      },
+    ],
 
-  instructions: {
-    type: String,
+    instructions: {
+      type: String,
+    },
+    subtotal: {
+      type: Number,
+      default: 0,
+    },
+    orderStatus: {
+      type: String,
+    },
+    paymentStatus: {
+      type: String,
+    },
   },
-  subtotal: {
-    type: Number,
-    default: 0,
-  },
-  orderStatus: {
-    type: String,
-  },
-  paymentStatus: {
-    type: String,
-  },
-
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", OrderSchema);
