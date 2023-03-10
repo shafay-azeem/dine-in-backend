86591e8745bd97da2d8181c7478520bc22897e98
shafay-azeem <shafayazeem12@gmail.com>
2023-03-10 19:40:07 +0500

    updated

--- backend/controller/TableController.js
+++ backend/controller/TableController.js
@@ -2,49 +2,49 @@ const asyncHandler = require("express-async-handler");
 const Table = require("../models/TableModel");
 
 exports.tableCreate = asyncHandler(async (req, res, next) => {
-  try {
-    const { tablesCount } = req.body;
-    if (isNaN(tablesCount) || tablesCount < 1) {
-      res.status(400).send("Invalid input");
-    } else {
-      const table = new Table({
-        userId: req.user._id,
-        TableNumber: tablesCount,
-      });
-      await table.save();
-    }
+    try {
+        const { tablesCount } = req.body;
+        if (isNaN(tablesCount) || tablesCount < 1) {
+            res.status(400).send("Invalid input");
+        } else {
+            const table = new Table({
+                userId: req.user._id,
+                TableNumber: tablesCount,
+            });
+            await table.save();
+        }
 
-    res.status(201).json({
-      success: true,
-      message: "Tables Rigesterd Successfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: "Failed to create tables",
-      error: error.message,
-    });
-  }
+        res.status(201).json({
+            success: true,
+            message: "Tables Rigesterd Successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Failed to create tables",
+            error: error.message,
+        });
+    }
 });
 
 exports.getTablebyUserId = asyncHandler(async (req, res, next) => {
-  let tablesCount = await Table.findOne({ userId: { $in: req.user.id } });
-  let count = tablesCount.TableNumber;
+    let tablesCount = await Table.findOne({ userId: { $in: req.user.id } });
+    let count = tablesCount.TableNumber;
 
-  console.log(tablesCount);
-  const tables = [];
-  for (let i = 1; i <= count; i++) {
-    const table = {
-      userId: req.user._id,
-      TableNumber: { "Table Number": i },
-    };
+    console.log(tablesCount);
+    const tables = [];
+    for (let i = 1; i <= count; i++) {
+        const table = {
+            userId: req.user._id,
+            TableNumber: i,
+        };
 
-    tables.push(table);
-  }
+        tables.push(table);
+    }
 
-  res.status(200).json({
-    success: true,
-    message: "Tables Get Successfully",
-    tables: tables,
-  });
+    res.status(200).json({
+        success: true,
+        message: "Tables Get Successfully",
+        tables: tables,
+    });
 });
