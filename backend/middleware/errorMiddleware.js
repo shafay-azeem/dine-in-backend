e4c1360a5f96b810d3601c1f25d984e41ba7ff04
shafay-azeem <shafayazeem12@gmail.com>
2023-03-13 15:01:58 +0500

    updated

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,5 +1,5 @@
 const notFound = (req, res, next) => {
-  console.log('jjee')
+  // console.log('jjee')
   const error = new Error(`Not Found - ${req.originalUrl}`);
   res.status(404);
   next(error);
@@ -7,7 +7,7 @@ const notFound = (req, res, next) => {
 
 
 const errorHandler = (error, req, res, next) => {
-  console.log(error)
+  // console.log(error)
   const status = error.statusCode || 500
   const message = error.message
   return res.status(status).json({ message: message })
